0d6c4d1ff607c173a9f033c6da6a46bfcd3528a5
mtintti <tiina.mantykivi@gmail.com>
2026-05-14 18:24:33 +0300

    loggertoFileTerminalAndSchemas

---
+++ backend/app/schemas/camera.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, AnyUrl
+
+# pedantic data validointi REST API kutsuille, kun api requesti tapahtuu pedantic:illa
+# saadaan vain halutut arvot apista. Pääkutsut jota käytetään json datan alkuun tapahtuvat MainObj luokalla
+# MainObj luokassa päästään omiin nested object kutsuihin rajaamaan Mainobject:in alempia json responceja (esim. CameraMainObject { id: string, geometry (CameraCoord): { coordinates: [xxx.xxx.xxx] })
+
+class CameraCoord(BaseModel):
+    coordinates: tuple[float, float, float]
+
+# kameran id url osoite kuviin, käytetään datasetissä ML modeliin
+class CameraPresetsImg(BaseModel):
+    id: str
+    presentationName: str
+    resolution: str
+    imageUrl: AnyUrl
+
+# properties json kutsussa, saadaan lähimmät kamerat ja Properties sisältää nested_object Presets -> url kuviin
+class CameraProp(BaseModel):
+    id: str
+    name: str
+    nearestWeatherStationId: int
+    municipality: str
+    presets: list[CameraPresetsImg]
+
+# jos tiedetään ennestään validi json structuuri, voidaan suoraan asettaa <nimi_json> : class_structuuri_nimi
+# eikä <json_nimi> : list[class_structuuri_nimi]
+class CameraObjMain(BaseModel):
+    id: str
+    geometry: CameraCoord
+    properties: CameraProp
+    #presets: list[dict[CameraPresetsImg]]
+
--- backend/app/services/fintraffic.py
+++ backend/app/services/fintraffic.py
@@ -1,8 +1,9 @@
 import httpx
 from fastapi import HTTPException, status
 from pydantic import ValidationError
-
-from ..dependecies import CameraObjMain, StationObjMain
+from ..schemas.camera import CameraObjMain
+from ..schemas.stations import StationObjMain
+from ..config import logger, loggercrier
 
 # itse REST API kutsut. clientistä saadaan api base + haluttu endpoint.
 # saatu response Data validoidaan/parsataan vain tarvittaviin arvoihin (parts_needed),
@@ -12,45 +13,38 @@ async def camera_station(camera_id: str, client:httpx.AsyncClient):
     try:
         response = await client.get(f"/stations/{camera_id}")
         response.raise_for_status()   # raises on 4xx/5xx
-        #return response.json()
         try:
             partNeeded = CameraObjMain.model_validate(response.json())
-            print(partNeeded)
-            print(" ")
-            if ValidationError:
-                print(ValueError)
+            logger.info("camera_id API call Pydantic validation happened, ", extra={"camera_id":camera_id})
             return partNeeded
         except ValidationError as er:
-            print(er)
+            loggercrier.error("API response Pedantic validation or parse ERROR", exc_info=True)
 
     except httpx.TimeoutException:
+        loggercrier.error("Api response took too long timeout, check .log", exc_info=True)
         raise HTTPException(status_code=status.HTTP_504_GATEWAY_TIMEOUT, detail="Digitraffic API timed out")
     except httpx.HTTPStatusError as e:
         if e.response.status_code == 404:
+            loggercrier.error("API was not found, check .log", exc_info=True)
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Camera {camera_id} not found")
+        loggercrier.error("Api response 502-gateway, check .log", exc_info=True)
         raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY, detail="Digitraffic API error")
 
 async def all_stations(client: httpx.AsyncClient):
-    #client = await get_client()
-
     try:
         response = await client.get(f"/stations")
-        print("\n in fintraffic all stations api call, res: ")
-        print(response)
         response.raise_for_status()   # raises on 4xx/5xx
-        #return response.json()
         try:
             partNeeded = StationObjMain.model_validate(response.json())
-            print(partNeeded)
-            print(" ")
-            if ValidationError:
-                print(ValueError)
             return partNeeded
         except ValidationError as er:
-            print(er)
+            loggercrier.error("API response Pedantic validation or parse ERROR", exc_info=True)
     except httpx.TimeoutException:
+        loggercrier.error("Api response took too long, check .log", exc_info=True)
         raise HTTPException(status_code=status.HTTP_504_GATEWAY_TIMEOUT, detail="Digitraffic API timed out")
     except httpx.HTTPStatusError as e:
         if e.response.status_code == 404:
+            loggercrier.error("API was not found, check .log", exc_info=True)
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Stations not found")
+        loggercrier.error("Api response 502-gateway, check .log", exc_info=True)
         raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY, detail="Digitraffic API error")
